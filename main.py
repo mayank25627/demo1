423733ce49ddbd84c5b7f6d62932c860b7445005
mayank25627 <sahu25627@gmail.com>
2024-04-02 23:59:23 +0530

    second cmmit

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 a = 10
 print
+
+
+# git init
+# git add .
+# git status
+
+# git commit -m "first commit"
+
+# git branch -M main
+
+# git remote add origin https://github.com/mayank25627/demo1.git
+
+# git push -u origin main
